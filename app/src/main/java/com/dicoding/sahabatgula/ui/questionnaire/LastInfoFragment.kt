e2f15f4759e209dec2cd78647194f8be9ce432be
mhmadilham18 <f1d022061@student.unram.ac.id>
2024-12-05 07:28:31 +0800

    add new layout, menu & navigation, and others dependencies

--- app/src/main/java/com/dicoding/sahabatgula/ui/questionnaire/LastInfoFragment.kt
+++ app/src/main/java/com/dicoding/sahabatgula/ui/questionnaire/LastInfoFragment.kt
@@ -1,5 +1,12 @@
 package com.dicoding.sahabatgula.ui.questionnaire
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dicoding.sahabatgula.R
+
 /**
  * A simple [Fragment] subclass.
  * Use the [LastInfoFragment.newInstance] factory method to
@@ -12,10 +19,7 @@ class LastInfoFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -26,23 +30,5 @@ class LastInfoFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_last_info, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LastInfoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LastInfoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
